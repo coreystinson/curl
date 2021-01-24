078352575055b2f70e66fe24b81e9270e5d19018
Corey Stinson <cstinson@smalljoes.com>
2021-01-23 22:33:32 -0600

    Adds batch files for project build.

---
+++ winbuild/build-x64-release-dyn.bat
@@ -0,0 +1,6 @@
+nmake /f Makefile.vc MODE=static DEBUG=no MACHINE=x64 ^
+WITH_ZLIB=static ^
+ZLIB_PATH=D:\Projects\lib\zlib-1.2.11\x64\release-dyn ^
+WITH_SSL=static ^
+SSL_PATH=D:\Projects\lib\openssl-1.1.1i\x64\release-dyn ^
+WITH_PREFIX=D:\Projects\lib\curl-7.74\x64\release-dyn
---
+++ winbuild/build-x64-release-static.bat
@@ -0,0 +1,6 @@
+nmake /f Makefile.vc MODE=static RTLIBCFG=static DEBUG=no MACHINE=x64 ^
+WITH_ZLIB=static ^
+ZLIB_PATH=D:\Projects\lib\zlib-1.2.11\x64\release-static ^
+WITH_SSL=static ^
+SSL_PATH=D:\Projects\lib\openssl-1.1.1i\x64\static-release ^
+WITH_PREFIX=D:\Projects\lib\curl-7.74\x64\release-static
